151b6c8c9f3d3e4ebf5e6f40403e0eb23becbf1f
ks8810as5086-ui <ks.8810.as.5086@gmail.com>
2026-08-05 12:37:14 +0900

    feat: 書籍一覧画面を実装

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BookController::class, 'index'])
+    ->name('books.index');
+
+Route::get('/books/create', [BookController::class, 'create'])
+    ->middleware('auth')
+    ->name('books.create');
+
+Route::get('/books/{book}', [BookController::class, 'show'])
+    ->name('books.show');
+
+Route::get('/ranking', function () {
+    return view('ranking.index');
+})->name('ranking.index');
+
+Route::get('/favorites', function () {
+    return view('favorites.index');
+})->middleware('auth')->name('favorites.index');
+
+Route::get('/genres', function () {
+    return view('genres.index');
+})->middleware('auth')->name('genres.index');
