754d6f95aeeefd908fdb0e05a42578e48b09ed1e
ks8810as5086-ui <ks.8810.as.5086@gmail.com>
2026-08-23 00:37:44 +0900

    feat: implement book store API

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::get('/v1/books', [BookController::class, 'index']);
 Route::get('/v1/books/{book}', [BookController::class, 'show']);
+Route::post('/v1/books', [BookController::class, 'store']);
